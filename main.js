e61f4f5a4b035ee06b9dc643a8b7a2e3374e7ac5
Nikola Grujin <nikola.grujin@ontariotechu.net>
2024-11-26 05:54:02 -0500

    Added forge and bucket models, implemented linearly interpolated movement, proper shadows, and added quenching oil and forge heat effects.

--- main.js
+++ main.js
@@ -12,67 +12,166 @@ document.body.appendChild(renderer.domElement);
 
 //Configuring scene and renderer
 renderer.setClearColor(0xfcba03);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 scene.background = new THREE.Color("#fcba03");
+
+//Creating clock object
+const clock = new THREE.Clock();
 
 //Creating model loader object
 const model_loader = new GLTFLoader();
 
 //Creating game objects
-const light = new THREE.DirectionalLight(0xffffff, 0.5);
+const sun_light = new THREE.DirectionalLight(0xffffff, 5);
+const ambient_light = new THREE.AmbientLight(0xffffff, 1);
 
-const floor_geo = new THREE.PlaneGeometry(10, 10);
-const floor_mat = new THREE.MeshBasicMaterial({color: "#fcba03"});
+const floor_geo = new THREE.PlaneGeometry(30, 30);
+const floor_mat = new THREE.MeshStandardMaterial({color: 0x757573, side: THREE.DoubleSide});
 const floor_plane = new THREE.Mesh(floor_geo, floor_mat);
+
+const forge_light_geo = new THREE.BoxGeometry(3, 1.5, 4);
+const forge_light_mat = new THREE.MeshStandardMaterial({emissive: new THREE.Color(0xff8400), emissiveIntensity: 1.0, transparent: true, opacity: 0.8, color: 0x000000});
+const forge_light = new THREE.Mesh(forge_light_geo, forge_light_mat);
+
+const quench_oil_geo = new THREE.CircleGeometry(1.5, 32);
+const quench_oil_mat = new THREE.MeshStandardMaterial({color: 0x875b08, transparent: true, opacity: 0.85, side: THREE.DoubleSide});
+const quench_oil = new THREE.Mesh(quench_oil_geo, quench_oil_mat);
 
 const hammer = new THREE.Object3D();
 const anvil = new THREE.Object3D();
+const forge = new THREE.Object3D();
+const bucket = new THREE.Object3D();
 
 //Configuring and positioning game objects
-floor_plane.position.y = -1.0;
-hammer.castShadow = true;
-hammer.position.x = -1.0;
-anvil.castShadow = true;
-anvil.position.x = 1.0;
+sun_light.position.set(0, 20, 0);
+sun_light.rotation.set(0, 0, 0);
+sun_light.castShadow = true;
+sun_light.shadow.mapSize.width = 512;
+sun_light.shadow.mapSize.height = 512;
+sun_light.shadow.camera.near = 0.5;
+sun_light.shadow.left = -10;
+sun_light.shadow.right = 10;
+sun_light.shadow.top = 10;
+sun_light.shadow.bottom = -10;
+sun_light.shadow.camera.far = 200;
 
-//Position camera
-camera.position.y = 5.0;
-camera.position.z = 2.0;
+floor_plane.position.set(0, -6, 0);
+floor_plane.rotation.x = Math.PI / 2;
+floor_plane.receiveShadow = true;
+
+forge_light.position.set(0, -1.25, 7.1);
+
+quench_oil.position.set(6, -3.2, 0);
+quench_oil.rotation.x = Math.PI / 2;
+
+hammer.castShadow = true;
+hammer.position.set(-2, -2, -4);
+hammer.rotation.set(-Math.PI / 12, 0, Math.PI);
+
+anvil.castShadow = true;
+anvil.position.set(0, -5, -5);
+anvil.rotation.set(0, Math.PI / 2, 0);
+
+forge.castShadow = true;
+forge.position.set(0, -7, 7);
+
+bucket.castShadow = true;
+bucket.position.set(6, -6.3, 0);
+
+//Configure, position, and rotate camera
+camera.rotation.order = "YXZ";
+camera.rotation.set(-0.2, 0, 0);
 
 //Loading models and parenting them to their corresponding objects
 LoadModel("models/hammer.glb", hammer);
 LoadModel("models/anvil.glb", anvil);
+LoadModel("models/forge.glb", forge);
+LoadModel("models/bucket.glb", bucket);
 
 //Adding game objects to the scene
-scene.add(light);
+scene.add(sun_light);
+scene.add(ambient_light);
+scene.add(forge_light);
+scene.add(quench_oil);
 scene.add(floor_plane);
 scene.add(hammer);
 scene.add(anvil);
+scene.add(forge);
+scene.add(bucket);
 
-//Debug key events for camera rotation
-document.addEventListener("keydown", (e) => {
-    switch(e.key)
+//Creating variables for tracking current view and animation state
+const workstation = {id: 0, focused: false, lerp_alpha: 0, rot_lerp_alpha: 0, rot_start: 0, rot_end: 0};
+
+//Creating a raycaster and mouse position vector
+const mouse_pos = new THREE.Vector2(0, 0);
+const raycaster = new THREE.Raycaster();
+
+//Click event listener
+document.addEventListener("click", (e) => {
+    mouse_pos.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse_pos.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    if(mouse_pos.x <= -0.8 && mouse_pos.x >= -1 && !workstation.focused && workstation.lerp_alpha == 0 && workstation.rot_lerp_alpha == 1)
     {
-        case " ":
-            console.log("x: " + camera.rotation.x + " ,y: " + camera.rotation.y);
-            break;
-        case "ArrowUp":
-            camera.rotation.x += 0.5;
-            break;
-        case "ArrowDown":
-            camera.rotation.x -= 0.5;
-            break;
-        case "ArrowLeft":
-            camera.rotation.y += 0.5;
-            break;
-        case "ArrowRight":
-            camera.rotation.y -= 0.5;
-            break;
+        workstation.rot_start = workstation.id * (Math.PI / 2);
+        workstation.id++;
+        workstation.rot_end = workstation.id * (Math.PI / 2);
+        if(workstation.id > 3) workstation.id = 0;
+        workstation.rot_lerp_alpha = 0;
+    }
+    else if(mouse_pos.x >= 0.8 && mouse_pos.x <= 1 && !workstation.focused && workstation.lerp_alpha == 0 && workstation.rot_lerp_alpha == 1)
+    {
+        workstation.rot_start = workstation.id * (Math.PI / 2);
+        workstation.id--;
+        workstation.rot_end = workstation.id * (Math.PI / 2);
+        if(workstation.id < 0) workstation.id = 3;
+        workstation.rot_lerp_alpha = 0;
+    }
+    else if(mouse_pos.y <= -0.5 && mouse_pos.y >= -1)
+    {
+        workstation.focused = !workstation.focused;
     }
 });
 
 //Update loop
 function update()
 {
+    //Get deltatime
+    const deltatime = clock.getDelta();
+
+    //Update and clamp lerp alpha
+    workstation.lerp_alpha += 0.9 * deltatime * (workstation.focused ? 1 : -1);
+    workstation.lerp_alpha = Math.min(Math.max(workstation.lerp_alpha, 0), 1);
+
+    //Update and clamp rotation lerp alpha
+    workstation.rot_lerp_alpha += 0.9 * deltatime;
+    workstation.rot_lerp_alpha = Math.min(Math.max(workstation.rot_lerp_alpha, 0), 1);
+
+    //Check workstation id, interpolate if required
+    const workstation_pos = new THREE.Vector3(0, 0, 0);
+    const workstation_rot = new THREE.Vector3(0, 0, 0);
+    switch(workstation.id)
+    {
+        case 0:
+            workstation_pos.set(0, 0, -2);
+            break;
+        case 1:
+            workstation_pos.set(-2, 0, 0);
+            break;
+        case 2:
+            workstation_pos.set(0, 0, 2);
+            break;
+        case 3:
+            workstation_pos.set(2, 0, 0);
+            break;
+    }
+    workstation_rot.lerpVectors(new THREE.Vector3(-0.2, 0, 0), new THREE.Vector3(-0.8, 0, 0), workstation.lerp_alpha);
+    camera.rotation.x = workstation_rot.x;
+    workstation_rot.lerpVectors(new THREE.Vector3(0, workstation.rot_start, 0), new THREE.Vector3(0, workstation.rot_end, 0), workstation.rot_lerp_alpha);
+    camera.rotation.y = workstation_rot.y;
+    camera.position.lerpVectors(new THREE.Vector3(0, 0, 0), workstation_pos, workstation.lerp_alpha);
+
     //Render the scene to the camera
     renderer.render(scene, camera);
 }
@@ -83,7 +182,11 @@ function LoadModel(model_path, parent)
 {
     model_loader.load(model_path,
         (gltf) => {
-            parent.add(gltf.scene);
+            let model = gltf.scene;
+            model.traverse((child) => {
+                if(child.isMesh) child.castShadow = true;
+            });
+            parent.add(model);
         },
         undefined,
         (error) => {
