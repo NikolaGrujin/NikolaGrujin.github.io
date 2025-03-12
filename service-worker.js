44da9f61ccabe0451bd3b82b84be5bcce46ac1dd
Nikola Grujin <ngrujin@outlook.com>
2025-03-12 10:39:53 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "radioport-v1-cache";
+const FILES_TO_CACHE = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-256.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(FILES_TO_CACHE);
+  }));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => {
+    return response || fetch(event.request);
+  }));
+});
